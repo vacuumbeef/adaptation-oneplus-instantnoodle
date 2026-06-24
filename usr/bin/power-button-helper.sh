6695f52a1a3119194546217bd7f6c64521a7d88f
vacuumbeef <aashcap@disroot.org>
2026-06-24 20:12:38 +0300

    new power button script

--- usr/bin/power-button-helper.sh
+++ usr/bin/power-button-helper.sh
@@ -2,36 +2,17 @@
 
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/32011/bus"
 
-CACHE_FILE="/home/droidian/.cache/backlight-brightness"
-#CACHE_FILE="/tmp/backlight-brightness"
 BRIGHTNESS_FILE="/sys/class/backlight/panel0-backlight/brightness"
-MAX_BRIGHTNESS_FILE="/sys/class/backlight/panel0-backlight/max_brightness"
-MIN_BRIGHTNESS=20
-DEFAULT_BRIGHTNESS=500
 
-mkdir -p /home/droidian/.cache
-
-saved_brightness=$DEFAULT_BRIGHTNESS
-val=$(cat "$CACHE_FILE" 2>/dev/null)
-if [[ "$val" -ge "$MIN_BRIGHTNESS" ]] 2>/dev/null; then
-	saved_brightness=$val
-fi
+saved_percent=$(gdbus call --session --dest org.gnome.SettingsDaemon.Power --object-path /org/gnome/SettingsDaemon/Power --method org.freedesktop.DBus.Properties.Get org.gnome.SettingsDaemon.Power.Screen Brightness | awk '{print $NF}' | tr -cd '0-9')
 
 stdbuf -oL dbus-monitor --session "type='signal',interface='org.gnome.ScreenSaver',member='ActiveChanged'" | while read -r line; do
 	if [[ "$line" == *"boolean false"* ]]; then
-		echo "$saved_brightness" > "$BRIGHTNESS_FILE"
+		# Wake up
+		gdbus call --session --dest org.gnome.SettingsDaemon.Power --object-path /org/gnome/SettingsDaemon/Power --method org.freedesktop.DBus.Properties.Set org.gnome.SettingsDaemon.Power.Screen Brightness "<int32 $saved_percent>" >/dev/null 2>&1
 
 	elif [[ "$line" == *"boolean true"* ]]; then
-		gsd_power_percentage=$(gdbus call --session --dest org.gnome.SettingsDaemon.Power --object-path /org/gnome/SettingsDaemon/Power --method org.freedesktop.DBus.Properties.Get org.gnome.SettingsDaemon.Power.Screen Brightness | awk '{print $NF}' | tr -cd '0-9')
-		
-		if [[ "$gsd_power_percentage" -gt 0 ]] 2>/dev/null; then
-			max_backlight=$(cat "$MAX_BRIGHTNESS_FILE")
-			
-			saved_brightness=$(( (gsd_power_percentage * max_backlight) / 100 ))
-			
-			if [[ "$saved_brightness" -ge "$MIN_BRIGHTNESS" ]] 2>/dev/null; then
-				echo "$saved_brightness" > "$CACHE_FILE"
-			fi
-		fi
+		# Sleep
+		saved_percent=$(gdbus call --session --dest org.gnome.SettingsDaemon.Power --object-path /org/gnome/SettingsDaemon/Power --method org.freedesktop.DBus.Properties.Get org.gnome.SettingsDaemon.Power.Screen Brightness | awk '{print $NF}' | tr -cd '0-9')
 	fi
 done
